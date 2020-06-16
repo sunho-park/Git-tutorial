d4468a2365f0418dfe2e1b8cb360511fb7e55338
sunho-park <reebox22@hanmail.net>
2020-06-16 20:51:38 +0900

    add my project

---
+++ myproject/sc3.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 사진 데이터 읽어들이기
+photos = np.load('myproject/photos.npz')
+x = photos['x']
+y = photos['y']
+
+# 시작 인덱스 --( 1)
+idx = 0
+
+#pyplot로 출력하기
+plt.figure(figsize=(10, 10))
+
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.title(y[i+idx])
+    plt.imshow(x[i+idx])
+plt.show()
---
+++ myproject/sc6_OpenCV.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import cv2 # OpenCV 사용하기
+import numpy as np
+
+# 사진 데이터 읽어 들이기
+photos = np.load('myproject/photos.npz')
+x = photos['x']
+img = x[12] # 사진 하나 선택하기
+
+plt.figure(figsize=(10, 10))
+for i in range(36):
+    plt.subplot(6, 6, i+1)
+    # 회전하기
+    center = (16, 16) # 회전 중심
+    angle = i*5       # 각도 지정
+    scale = 1.0       # 확대 비율 지정
+    mtx = cv2.getRotationMatrix2D(center, angle, scale)
+    img2 = cv2.warpAffine(img, mtx, (32, 32))
+    # 화면에 출력하기
+    plt.imshow(img2)
+plt.show()
+
+
+
