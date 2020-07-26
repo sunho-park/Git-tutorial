7662dcdf6d7035e09528f4c0ad92f4badb114d54
sunho-park <reebox22@hanmail.net>
2020-07-26 20:40:35 +0900

    Add

--- nomad/202face_detection_video.py
+++ nomad/202face_detection_video.py
@@ -2,3 +2,14 @@ import cv2
 import numpy as np
 from tkinter import *
 
+'''
+face_Cascade = 
+
+aramghae typpwdmamsdmfafmskldfskmldfmlfmawelfm
+
+sdf;asmf's;dfm';sfmsfm'awmefmawe'fmawelmfawl'mfaw
+fwmfwe'mf'wlefmalewfmawlefmlefmawel'fkmawelfmaw
+wflkew'mfwlemfa'wlfmwal'efmawlmfe
+fwl'emfaw'emf'lawemfw'elmfwafmaw'lef
+wefmkaw'elfma'wmlfweflm
+'''
